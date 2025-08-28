9559d4815ecf69af76120d5e94b5f0959c3e9e0b
Bipul <123935352+Syed-Bipul-Rahman@users.noreply.github.com>
2025-08-28 19:08:22 +0600

    refactor: Use a base path for icons in AppIcons

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -1,17 +1,18 @@
 abstract class AppIcons {
   AppIcons._();
-  static const String albumsIcon = 'assets/icons/albums_icon.svg';
-  static const String burger = 'assets/icons/burger.svg';
-  static const String clickSingleImage = 'assets/icons/click_single_image.svg';
+  static const String _basePath = 'assets/icons';
+  static const String albumsIcon = '$_basePath/albums_icon.svg';
+  static const String burger = '$_basePath/burger.svg';
+  static const String clickSingleImage = '$_basePath/click_single_image.svg';
   static const String copyIconThatRepresentMultiClick =
-      'assets/icons/copy_icon_that_represent_multi_click.svg';
-  static const String flashOff = 'assets/icons/flash_off.svg';
-  static const String flashOn = 'assets/icons/flash_on.svg';
-  static const String multiClickImage = 'assets/icons/multi_click_image.svg';
-  static const String person = 'assets/icons/person.svg';
-  static const String showLessItems = 'assets/icons/show_less_items.svg';
-  static const String showMoreItem = 'assets/icons/show_more_item.svg';
-  static const String switchCamera = 'assets/icons/switch_camera.png';
-  static const String timerOff = 'assets/icons/timer_off.svg';
-  static const String timerOn = 'assets/icons/timer_on.svg';
+      '$_basePath/copy_icon_that_represent_multi_click.svg';
+  static const String flashOff = '$_basePath/flash_off.svg';
+  static const String flashOn = '$_basePath/flash_on.svg';
+  static const String multiClickImage = '$_basePath/multi_click_image.svg';
+  static const String person = '$_basePath/person.svg';
+  static const String showLessItems = '$_basePath/show_less_items.svg';
+  static const String showMoreItem = '$_basePath/show_more_item.svg';
+  static const String switchCamera = '$_basePath/switch_camera.png';
+  static const String timerOff = '$_basePath/timer_off.svg';
+  static const String timerOn = '$_basePath/timer_on.svg';
 }
